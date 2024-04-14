90f6d6ff530e66ed3d51cc0714f87b9acfc687f4
Longs Pemun Gotar <developerlongs@gmail.com>
2024-04-14 14:07:36 +0100

    fix: return

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 type Mapper struct {
-	Mapping map[string]string
-	Lock sync.Mutex
+    Mapping map[string]string
+    Lock    sync.Mutex
 }
 
 var urlMapper Mapper
@@ -23,71 +23,65 @@ func init() {
 }
 
 func main() {
-	r := chi.NewRouter()
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
 
-	r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Server running..."))
+    })
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Server running..."))
-	})
+    r.Post("/short-it", createShortURLHandler)
+    r.Get("/short/{key}", redirectHandler)
 
-	r.Post("/short-it", createShortURLHandler)
-	r.Get("/short/{key}", redirectHandler)
-
-	http.ListenAndServe(":8080", r)
+    http.ListenAndServe(":8080", r)
 }
 
-func createShortURLHandler(w http.ResponseWriter, r *http.Request){
-	r.ParseForm()
-	u := r.Form.Get("Url")
-	if u == "" {
+func createShortURLHandler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    u := r.Form.Get("URL")
+    if u == "" {
         w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("url field cannot be empty"))
-		return
-	}
-
-	w.Write([]byte("URL field is required"))
-
-	key := shortuuid.New()
-
-	insertMapping(key, u)
-
-	log.Println("url mapped successfully")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("http://localhost:8080/short/%s", key)))
-}
-
-func redirectHandler(w http.ResponseWriter, r *http.Request){
-	key := chi.URLParam(r, "key")
-	if key == "" {
-        w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("key field cannot be empty"))
-		return
-	}
-
-	u := fetchMapping(key)
-	if u == "" {
-        w.WriteHeader(http.StatusNotFound)
-        w.Write([]byte("url not found"))
+        w.Write([]byte("url field cannot be empty"))
+        return
     }
 
-	http.Redirect(w,r, u, http.StatusFound)
+    key := shortuuid.New()
+    insertMapping(key, u)
+    log.Println("url mapped successfully")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("http://localhost:8080/short/%s", key)))
 }
-func insertMapping(key string, u string){
-	urlMapper.Lock.Lock()
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+    key := chi.URLParam(r, "key")
+    if key == "" {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte("key field cannot be empty"))
+        return
+    }
+
+    u := fetchMapping(key)
+    if u == "" {
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte("url not found"))
+        return
+    }
+
+    http.Redirect(w, r, u, http.StatusFound)
+}
+
+func insertMapping(key string, u string) {
+    urlMapper.Lock.Lock()
     urlMapper.Mapping[key] = u
     defer urlMapper.Lock.Unlock()
 }
 
-func fetchMapping(key string) string{
-	urlMapper.Lock.Lock()
+func fetchMapping(key string) string {
+    urlMapper.Lock.Lock()
     defer urlMapper.Lock.Unlock()
-    key, ok := urlMapper.Mapping[key]
+    value, ok := urlMapper.Mapping[key]
     if ok {
-       return urlMapper.Mapping[key]
+        return value
     }
-
-	return ""
+    return ""
 }
-
